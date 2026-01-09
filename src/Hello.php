99efc5b3e53fca7a08b43a749d19c19195c74a7d
Theo <thveber@stpbb.org>
2026-01-09 13:58:32 +0100

    Ajout fonctions

---
+++ src/Hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Hello
+{
+    public function sayHello(string $name): string
+    {
+        return "Bonjour, " . $name . "!";
+    }
+}
---
+++ tests/HelloTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Hello;
+
+class HelloTest extends TestCase
+{
+    public function testSayHello(): void 
+    {
+        $hello = new Hello();
+        $this->assertSame("Bonjour, BTS SIO SLAM!", $hello->sayHello("BTS SIO SLAM"));
+    }
+
+    public function testSayHelloEmpty(): void 
+        {
+            $hello = new Hello();
+            $this->assertSame("Bonjour, !", $hello->sayHello(""));
+        }
+
+        public function testSpecialCharacters(): void 
+        {
+            $hello = new Hello();
+            $this->assertSame("Bonjour, {Èlodie!", $hello->sayHello("Èlodie"));
+        }
+
+        public function testNumericName(): void 
+        {
+            $hello = new Hello();
+            $this->assertSame("Bonjour, 12345!", $hello->sayHello("12345"));
+        }
+
+        public function testReturnType(): void 
+        {
+            $hello = new Hello();
+            $this->assertIsString($hello->sayHello("Test"));
+        }
+}
